bdff25fc90f10a893e22eab21b9c3bc5a2ff58a6
findingDodi <dorina.diem@gmail.com>
2023-08-04 16:19:59 +0200

    replaced factories, added vehicle

--- inc/GameLogic.py
+++ inc/GameLogic.py
@@ -19,16 +19,17 @@ class GameLogic:
         factory_types = [ResourceCoal, ResourceCoal, ResourceIron, ResourceSteelPlate]
         for i, factory_type in enumerate(factory_types):
             current_factory = BuildingFactory(factory_type)
-            current_factory.set_position((i + 1) * 100, (i + 1) * 100)
+            current_factory.set_position((i + 1) * 100, ((i + 1) % 3) * 100)
             self.buildings.append(current_factory)
 
         new_parking_lot = BuildingParkingLot()
-        new_vehicle_truck = VehicleTruck()
-        new_vehicle_truck.set_position(new_parking_lot.position_x, new_parking_lot.position_y)
-        new_vehicle_truck.set_destination(random.choice(self.buildings))
-
         self.buildings.append(new_parking_lot)
-        self.vehicles.append(new_vehicle_truck)
+
+        for i in range(2):
+            new_vehicle_truck = VehicleTruck()
+            new_vehicle_truck.set_position(new_parking_lot.position_x, new_parking_lot.position_y)
+            new_vehicle_truck.set_destination(random.choice(self.buildings))
+            self.vehicles.append(new_vehicle_truck)
 
     def game_tick(self):
         self.move_vehicles()
